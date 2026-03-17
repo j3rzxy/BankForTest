e254f669b089357d8e65b1fdc2c92d2f2b9838af
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2026-03-17 19:06:12 +0300

    Создан проект для теста.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
