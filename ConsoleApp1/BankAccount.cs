54e85c35abfaea44d268a7ec4391dee453c6c81e
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2026-03-17 18:48:52 +0300

    Приложение задокументировано.

--- ConsoleApp1/BankAccount.cs
+++ ConsoleApp1/BankAccount.cs
@@ -4,12 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace BankAccountNS
 {
-    internal class BankAccount
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Поле, содержащее ФИО клиента.
+        /// </summary>
+        private readonly string m_customerName;
+        /// <summary>
+        /// Поле, содержащее баланс счета клиента.
+        /// </summary>
+        private double m_balance;
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        private BankAccount() { }
+        /// <summary>
+        /// Базовый конструктор BankAccount создает ФИО и баланс счета клиента.
+        /// </summary>
+        /// <param name="customerName">ФИО клиента.</param>
+        /// <param name="balance">Баланс счета клиента.</param>
+        public BankAccount(string customerName, double balance)
         {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+        /// <summary>
+        /// Свойство CustomerName получает ФИО клиента.
+        /// </summary>
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        /// <summary>
+        /// Свойство Balance получает значение баланса клиента.
+        /// </summary>
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        /// <summary>
+        /// Метод Debit, который вызывается, когда денежные средства снимаются со счета.
+        /// </summary>
+        /// <param name="amount">Сумма денежных средств, которые требуется снять.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Исключение возникающее при отрицательном значении суммы и/или превышении баланса.</exception>
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+        /// <summary>
+        /// Метод Credit, который вызывается, когда денежные средства поступают на счет.
+        /// </summary>
+        /// <param name="amount">Сумма денежных средств, которые требуется начислить.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Исключение возникающее при отрицательном значении суммы.</exception>
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+        /// <summary>
+        /// Главная функция Main.
+        /// </summary>
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+            Console.ReadLine();
         }
     }
 }
