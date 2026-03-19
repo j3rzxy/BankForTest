94941ee67a3db1eba4ec7431cfeb5d35ec4bfa55
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2026-03-19 18:36:17 +0300

    Созданы тесты для проверки корректности второго метода Credit.

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -56,5 +56,32 @@ namespace BankTests
 
             Assert.Fail("The expected exception was not thrown.");
         }
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 2.01;
+            double expected = 14.00;
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            // Act
+            account.Credit(creditAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+        [TestMethod]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Credit(creditAmount));
+        }
     }
 }
