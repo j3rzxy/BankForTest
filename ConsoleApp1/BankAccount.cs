2ad8fa8f41906170d8089bb9c55162bcb4215c3d
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2026-03-19 18:30:34 +0300

    Проведен рефакторинг кода программы и улучшены тесты.

--- ConsoleApp1/BankAccount.cs
+++ ConsoleApp1/BankAccount.cs
@@ -11,13 +11,9 @@ namespace BankAccountNS
     /// </summary>
     public class BankAccount
     {
-        /// <summary>
-        /// Поле, содержащее ФИО клиента.
-        /// </summary>
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
         private readonly string m_customerName;
-        /// <summary>
-        /// Поле, содержащее баланс счета клиента.
-        /// </summary>
         private double m_balance;
         /// <summary>
         /// Конструктор по умолчанию.
@@ -57,12 +53,12 @@ namespace BankAccountNS
         {
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
             m_balance -= amount;
